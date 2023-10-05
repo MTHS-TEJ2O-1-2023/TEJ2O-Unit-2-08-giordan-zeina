ff6e0afd0ec55f9cd13a928c7c9a1192deab0846
giordan-zeina <144264315+giordan-zeina@users.noreply.github.com>
2023-10-05 12:49:35 -0400

    Update pxt.json, main.ts, README.md

--- main.ts
+++ main.ts
@@ -1,8 +1,40 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2023 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Giordan Zeina
+ * Created on: OCT 2023
+ * This program when the user clicks the "A" button, the Micro:bit powers on a single RGB LED on a breadboard and scrolls through the colors it can produce
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+pins.digitalWritePin(DigitalPin.P13,0)
+pins.digitalWritePin(DigitalPin.P14,0)
+pins.digitalWritePin(DigitalPin.P15,0)
+basic.showIcon(IconNames.Happy)
+basic.clearScreen()
+
+input.onButtonPressed(Button.A, function() {
+  basic.showString("Red")
+  pins.digitalWritePin(DigitalPin.P13,1)
+  basic.pause(1000)
+  basic.showString("Blue")
+  pins.digitalWritePin(DigitalPin.P13,0)
+  pins.digitalWritePin(DigitalPin.P14,1)
+  basic.pause(1000)
+  basic.showString("Green")
+  pins.digitalWritePin(DigitalPin.P14,1)
+  pins.digitalWritePin(DigitalPin.P15,1)
+  basic.pause(1000)
+  basic.showString("Magenta")
+  pins.digitalWritePin(DigitalPin.P13, 0)
+  pins.digitalWritePin(DigitalPin.P13,1)
+  basic.pause(1000)
+  basic.showString("Yellow")
+  pins.digitalWritePin(DigitalPin.P13,0)
+  pins.digitalWritePin(DigitalPin.P15,1)
+  basic.pause(1000)
+  basic.showString("Cyan")
+  pins.digitalWritePin(DigitalPin.P14,1)
+  basic.pause(1000)
+  basic.showString("White")
+  pins.digitalWritePin(DigitalPin.P13,1)
+})
